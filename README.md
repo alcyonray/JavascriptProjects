5dbe0366d11645a23626b3f030a2340ee7c8192e
alcyonray <alcyonray@gmail.com>
2024-02-20 18:48:07 -0800

    First checkpoint commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository will contain all of the Javascript oriented files, for course work through the Tech Academy.
---
+++ basicJavaScriptProjects/Project1_expressionsAlert/JS/main.js
@@ -0,0 +1,13 @@
+var myString = "This is a string!"; //creating a string
+
+window.alert(myString); //putting that string in an alert window
+
+var anotherString = "Don't you agree?"; //making another string
+
+document.write("They say programming is \"fun\". " + anotherString); //concatenating 'anotherString' in a larger string, written in the html
+
+var myself = "Jeremiah", partner = "Mandey", childOne = "Sufi", childTwo = "Luca"; //creating multiple variables on the same line
+
+document.write("The members of my lovely little family are: " + partner + ", " + myself + ", " + childOne + ", and " + childTwo + "."); //concatenating all of those into a statement
+
+var myExpression = 2 + 3 + 1995; //making an expression
