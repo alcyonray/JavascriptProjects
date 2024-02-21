a73a2e0bdc3454ccad477fc02965c87ef4036ee1
alcyonray <alcyonray@gmail.com>
2024-02-20 18:52:26 -0800

    First assignment

--- basicJavaScriptProjects/Project1_expressionsAlert/JS/main.js
+++ basicJavaScriptProjects/Project1_expressionsAlert/JS/main.js
@@ -11,3 +11,7 @@ var myself = "Jeremiah", partner = "Mandey", childOne = "Sufi", childTwo = "Luca
 document.write("The members of my lovely little family are: " + partner + ", " + myself + ", " + childOne + ", and " + childTwo + "."); //concatenating all of those into a statement
 
 var myExpression = 2 + 3 + 1995; //making an expression
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string." //creating the Sent1 and Sent2 variables
+
+var secondExpression = Sent1 + Sent2; //concatenating them together in an expression
